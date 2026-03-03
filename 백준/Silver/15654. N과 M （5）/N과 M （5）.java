acf5ca593ec3c072055424f558aa37047e9c13d3
cutenabi0527 <lgs7042@gmail.com>
2026-03-03 17:31:54 +0900

    [Silver III] Title: N과 M (5), Time: 380 ms, Memory: 32232 KB -SsafyToday

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,52 @@
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main {
+	static int n, m;
+	static int[] arr;
+	static int[] num;
+	static StringBuilder sb = new StringBuilder();
+	static boolean[] visited;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		n = sc.nextInt();
+		m = sc.nextInt();
+		arr = new int[m];
+		num = new int[n+1];
+		visited = new boolean [n+1];
+		for (int i = 1; i <= n; i++) {
+			num[i] = sc.nextInt();
+		}
+		Arrays.sort(num);
+
+		dfs(0);
+
+		System.out.println(sb);
+
+	}
+
+	public static void dfs(int depth) {
+		if (depth == m) {
+			for (int val : arr) {
+				sb.append(val).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+
+		for (int i = 1; i <= n; i++) {
+			if (!visited[i]) {
+				visited[i]=true;
+				arr[depth] = num[i];
+				dfs(depth + 1);
+				visited[i]=false;
+			}
+			
+		}
+	}
+
+}
