729734d52f17ebcc46622b215b1c3c889db7c5b9
cutenabi0527 <lgs7042@gmail.com>
2026-03-03 17:48:29 +0900

    [Silver III] Title: N과 M (8), Time: 200 ms, Memory: 19840 KB -SsafyToday

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	static int n, m;
+	static int[] arr;
+	static int[] num;
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		n = sc.nextInt();
+		m = sc.nextInt();
+		arr = new int[m];
+		num = new int[n];
+		for (int i = 0; i < n; i++) {
+			num[i] = sc.nextInt();
+		}
+		Arrays.sort(num);
+
+		dfs(0, 0);
+
+		System.out.println(sb);
+
+	}
+
+	public static void dfs(int depth, int start) {
+		if (depth == m) {
+			for (int val : arr) {
+				sb.append(val).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+
+		for (int i = start; i < n; i++) {
+			arr[depth] = num[i];
+			dfs(depth + 1, i);
+			
+		}
+	}
+
+}
